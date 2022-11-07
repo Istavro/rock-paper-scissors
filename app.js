a8de55e93294574186800880e6a5f6e617215b48
Istavro Shakoori <Istavro@protonmail.com>
2022-11-08 09:51:56 +1100

    Improved UI and add Winner

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ let computerChoiceElement = document.getElementById("computerChoice");
 let userChoiceElement = document.getElementById("userChoice");
 let result = document.getElementById("result");
 let buttons = document.querySelectorAll("button");
+let winner = document.getElementById("winner");
+
+let scoreBoard = [0, 0]
+
+const updateScore = function(){
+    document.getElementById("userScore").textContent = scoreBoard[0];
+    document.getElementById("computerScore").textContent = scoreBoard[1];
+
+    if(scoreBoard[0] == 5){
+        winner.textContent = "Human wins!"
+    }
+    if(scoreBoard[1] == 5){
+        winner.textContent = "The robots are taking over!"
+    }
+}
 
 let outcomes = [
     {
@@ -19,22 +34,27 @@ let outcomes = [
 ];
 
 buttons.forEach(buttons => buttons.addEventListener('click', e => {
+
     let userChoice = buttons.innerText;
-    userChoiceElement.innerHTML = userChoice;
+    userChoiceElement.textContent = userChoice;
     let computerChoice = outcomes[Math.floor(Math.random() * outcomes.length)];
-    computerChoiceElement.innerHTML = computerChoice.choice;
+    computerChoiceElement.textContent = computerChoice.choice;
 
     //get results
     if (computerChoice.beats === userChoice){
-        result.innerHTML = "Computer wins"
+        result.textContent = "Computer wins"
+        scoreBoard[1]++
+        updateScore();
         return;
     }
 
     if (computerChoice.choice === userChoice){
-        result.innerHTML = "Draw"
+        result.textContent = "Draw"
         return;
     }
 
-    result.innerHTML = "User wins"
+    result.textContent = "User wins"
+    scoreBoard[0]++
+    updateScore();
     return;
   }))
